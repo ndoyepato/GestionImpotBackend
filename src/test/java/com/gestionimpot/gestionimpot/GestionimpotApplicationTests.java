c13791e0e13024a0510c8565fd394bbe03e6136a
Issa Ndoye Mbengue <issandmb19@gmail.com>
2020-01-30 00:04:06 +0000

    First commit, Created Spring Boot Project

---
+++ src/test/java/com/gestionimpot/gestionimpot/GestionimpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionimpot.gestionimpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionimpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
